3ae466ba5ad29db928d8b981032ba582dd4b9a49
ViliusU <75509545+ViliusU@users.noreply.github.com>
2025-07-30 21:51:38 +0300

    Update fan_control_install.sh

--- fan_control_install.sh
+++ fan_control_install.sh
@@ -55,11 +55,11 @@ EOF
 
 # Set execution permissions for the script
 echo "Setting execute permissions for fan_control.py..."
-sudo chmod +x /home/pi/fan_control.py
+sudo chmod +x /home/fan_control.py
 
 # Modify rc.local to run the script on boot
 echo "Configuring rc.local to run the script on startup..."
-sudo sed -i '$i \python3 /home/pi/fan_control.py &' /etc/rc.local
+sudo sed -i '$i \python3 /home/fan_control.py &' /etc/rc.local
 
 # Ensure rc.local is executable
 echo "Ensuring rc.local is executable..."
